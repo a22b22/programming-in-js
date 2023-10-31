4e43aed13728b8ed51f053b2e0de797ad67569ba
Alexander B <84710326+a22b22@users.noreply.github.com>
2023-10-31 13:14:12 +0100

    feat: add arrays

---
+++ 07-arrays/script.js
@@ -0,0 +1,69 @@
+console.log("Task 1");
+
+/*
+01) removeItem function
+Implement a removeItem function.
+
+ The first parameter is an array.
+ The second parameter is a number. It's the non-zero-based index of the item that should get deleted.
+ Remove the item and return an array.
+ Ensure that the original array was not mutated.
+// TODO: Implement the removeItem function
+*/
+
+function removeItem(array, number) {
+  let result;
+  result = array.slice();
+  result.splice(number - 1, 1);
+  return result;
+}
+
+const animals = ["Dog", "Cat", "Lion"];
+console.log(removeItem(animals, 1));
+// result should be: ["Cat", "Lion"]
+
+console.log(animals);
+// result should be still: ["Dog", "Cat", "Lion"]
+
+const fruits = ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"];
+console.log(removeItem(fruits, 3));
+// result should be: ["Watermelon", "Banana", "Kiwi", "Pineapple", "Apple"]
+
+console.log(fruits);
+// result should be still: ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"]
+
+/*
+03) sumOfCharacters function
+Implement a sumOfCharacters function.
+
+ The function has one parameter, which is an array.
+ Check the type of each array entry. If it's a string then store the count of characters.
+ The function returns the total sum of all characters.
+ */
+
+// TODO: Implement the sumOfCharacters function
+console.log("Task 2");
+
+function sumOfCharacters(array) {
+  let result = 0;
+  for (const iterator of array) {
+    if (typeof iterator === "string") {
+      result += iterator.length;
+    }
+  }
+  return result;
+}
+
+const arr1 = ["Luke", "Anakin", true, "Obi Wan", 333];
+console.log(sumOfCharacters(arr1));
+// result should be: 17
+
+const arr2 = [
+  "Code is",
+  "like humor",
+  ".",
+  "When you have",
+  "to explain it, it's bad!",
+];
+console.log(sumOfCharacters(arr2));
+// result should be: 55
