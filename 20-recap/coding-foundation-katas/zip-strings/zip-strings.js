32446529e79b639d4f331d3f640ea6a6286f6f97
Alexander B <84710326+a22b22@users.noreply.github.com>
2023-12-07 13:17:04 +0100

    feat: add solution for zip strings task

--- 20-recap/coding-foundation-katas/zip-strings/zip-strings.js
+++ 20-recap/coding-foundation-katas/zip-strings/zip-strings.js
@@ -1,3 +1,13 @@
 function zipStrings(strA, strB) {
-  // Your Code here
+  let result = "";
+  const maxLength = Math.max(strA.length, strB.length);
+  for (let index = 0; index < maxLength; index++) {
+    if (index < strA.length) {
+      result += strA[index];
+    }
+    if (index < strB.length) {
+      result += strB[index];
+    }
+  }
+  return result;
 }
